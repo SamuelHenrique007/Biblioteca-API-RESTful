fe421d5ad763955897ae920c2a683bdbd92b5147
yuuri8 <yuriborges1011@gmail.com>
2025-12-03 17:25:22 -0300

    cria estrutura da API

---
+++ api/biblioteca/urls.py
@@ -0,0 +1,17 @@
+# biblioteca/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # Rotas da API DRF
+    path('api/', include('catalogo.api_urls')),
+
+    path('', include('catalogo.urls')),  
+
+    # JWT
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
